0c541f34f44dd40630740781cb141ec3a0685843
mathpunch Official <ozzycouts51@gmail.com>
2025-02-19 03:11:33 -0600

    Update grabip.js

--- grabip.js
+++ grabip.js
@@ -1,4 +1,3 @@
-
 const ipifyAPI = "https://api.ipify.org?format=json";
 
 
@@ -24,34 +23,3 @@ async function sendToDiscord(ip) {
 
     const payload = {
         content: `IP Address: ${ip}`
-    };
-
-    try {
-        const response = await fetch(webhookURL, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(payload)
-        });
-
-        if (response.ok) {
-            console.log("IP sent to Discord successfully!");
-        } else {
-            console.error("Error sending IP to Discord:", response.statusText);
-        }
-    } catch (error) {
-        console.error("Error:", error);
-    }
-}
-
-
-async function main() {
-    const ip = await getIP();
-    if (ip) {
-        await sendToDiscord(ip);
-    }
-}
-
-
-main();
